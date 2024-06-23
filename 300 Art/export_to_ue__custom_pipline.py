17bbc28f8a4c226ce3d5b32decf3228104676430
Ygor Boiko Mikhailovich <113383645+Rrailnechex@users.noreply.github.com>
2024-06-23 21:52:58 +0300

    FIXED => no crash at least)

--- 300 Art/export_to_ue__custom_pipline.py
+++ 300 Art/export_to_ue__custom_pipline.py
@@ -2,8 +2,12 @@ import bpy
 import datetime
 import traceback
 
+def clear_log_file():
+    with open("export_to_ue__custom_pipline.log", "w") as f:
+        pass
+
 def log_to_file(message):
-    time_written = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    time_written = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
     stack_depth = len(traceback.extract_stack()) - 1 
     indentation = " " * (stack_depth * 1)  # x spaces per stack depth level
     log_message = f"{time_written}: {indentation}{message}"
@@ -46,12 +50,21 @@ def update_view_print(message):
     update_view()
     print(message)
     log_to_file(message)
+    
+# Select the object. I SAD: SELECT THE OBJECT
+def select_object_by_force(obj):
+    if obj.name in bpy.context.view_layer.objects:
+        obj.select_set(True)
+        bpy.context.view_layer.objects.active = obj
+    else:
+        bpy.context.view_layer.objects.active = obj
+        obj.select_set(True)
 
 
 
 
 def export_armature_with_its_geometry_to_ue(rig):
-    update_view_print(f"Exporting {rig.name}")
+    update_view_print(f"Exporting {rig.name} started")
 
     original_collection_of_the_rig = rig.users_collection[0]
     temporal_export_collection = bpy.data.collections.new(name="Export")
@@ -60,45 +73,54 @@ def export_armature_with_its_geometry_to_ue(rig):
 
     # add rig and its childs to the "Export" collection
     move_to_collection_with_nierarchy(rig, temporal_export_collection)
-    update_view_print(f"Moved {rig.name} to {temporal_export_collection.name}")
+    update_view_print(f"Hierarchy of {rig.name} moved to {temporal_export_collection.name}")
 
 
     for obj in temporal_export_collection.all_objects:
         # only for geometry
         if not (
-            hasattr(obj, 'type') 
+            obj is not None
+            and hasattr(obj, 'type') 
             and obj.type is not None 
             and obj.type == "MESH"
         ):
+            update_view_print(f"Skipping {obj.name if obj else 'object'} because it is not a mesh")
             continue
 
 
-        # Select the object
-        obj.select_set(True)
-        bpy.context.view_layer.objects.active = obj
+        select_object_by_force(obj)
+
+        update_view_print(f"Selected {obj.name}")
 
         # Make single user and apply visual transform
         bpy.ops.object.make_single_user(object=True, obdata=True)
         bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
         bpy.ops.object.visual_transform_apply()
+
+        update_view_print(f"Made single user and applied visual transform to {obj.name}")
         
         obj.data.name = f"{obj.name}__unique_mesh"
+
+        update_view_print(f"Renamed object data to {obj.data.name}")
 
         obj.select_set(False)
         deselect_everything()
 
-        update_view_print(f"Made unique user and applied visual transform to {obj.name}")
+        update_view_print(f"Finished for {obj.name}")
+
 
 
     # # Send to Unreal Engine all things from the "Export" collection
     # bpy.ops.wm.send2ue()
 
     # delete the "Export" collection
-    # bpy.data.collections.remove(temporal_export_collection) # here is the crash
+    # bpy.data.collections.remove(temporal_export_collection)
     move_to_collection_with_nierarchy(rig, original_collection_of_the_rig)
-    update_view_print(f"Exported rig and its childs {rig.name}")
+    update_view_print(f"Moved {rig.name} to {original_collection_of_the_rig.name}")
+    update_view_print(f"Exported rig and its childs {rig.name} finished")
 
     pass
+
 
 
 
@@ -106,6 +128,9 @@ def export_armature_with_its_geometry_to_ue(rig):
 def main():
     # Save the current Blender file
     bpy.ops.wm.save_mainfile()
+
+    clear_log_file()
+
     update_view_print("Saved the current Blender file")
 
     deselect_everything()
@@ -123,6 +148,8 @@ def main():
 
     # # Revert all changes to the Blender file
     # bpy.ops.wm.revert_mainfile()
+
+    update_view_print(f"SCRIPT FINISHED")
 
 
 
