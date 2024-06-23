5a8c54f9ae82529f69131df34bd34144d8a04a15
Ygor Boiko Mikhailovich <113383645+Rrailnechex@users.noreply.github.com>
2024-06-23 22:27:59 +0300

    dont_fix_normals added

--- 300 Art/export_to_ue__custom_pipline.py
+++ 300 Art/export_to_ue__custom_pipline.py
@@ -2,14 +2,27 @@ import bpy
 import datetime
 import traceback
 
+
+
+# constants ------------------------------------------------------------------
+DONT_FIX_NORMALS = "dont_fix_normals"
+
+
+
+
+# functions ------------------------------------------------------------------
+
+def tab():
+    return " " * 3
+
 def clear_log_file():
     with open("export_to_ue__custom_pipline.log", "w") as f:
         pass
 
 def log_to_file(message):
     time_written = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-    stack_depth = len(traceback.extract_stack()) - 1 
-    indentation = " " * (stack_depth * 1)  # x spaces per stack depth level
+    stack_depth = len(traceback.extract_stack()) - 1 - 10
+    indentation = tab() * stack_depth
     log_message = f"{time_written}: {indentation}{message}"
     
     with open("export_to_ue__custom_pipline.log", "a") as f:
@@ -50,7 +63,10 @@ def update_view_print(message):
     update_view()
     print(message)
     log_to_file(message)
-    
+
+def force_rename_collection(collection, name):
+    collection.name = name
+
 # Select the object. I SAD: SELECT THE OBJECT
 def force_select_object(obj):
     if obj.name in bpy.context.view_layer.objects:
@@ -59,6 +75,14 @@ def force_select_object(obj):
     else:
         bpy.context.view_layer.objects.active = obj
         obj.select_set(True)
+
+def fix_object_normals(obj):
+    force_select_object(obj)
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.mesh.select_all(action='SELECT')
+    bpy.ops.mesh.normals_make_consistent(inside=False)
+    bpy.ops.mesh.select_all(action='DESELECT')
+    bpy.ops.object.mode_set(mode='OBJECT')
 
 
 
@@ -86,11 +110,11 @@ def export_armature_with_its_geometry_to_ue(rig):
             and obj.type is not None 
             and obj.type == "MESH"
         ):
-            update_view_print(f"Skipping {obj.name if obj else 'object'} because it is not a mesh")
+            update_view_print(f"{tab()}Skipping {obj.name if obj else 'deez nust'}")
             continue
 
         meshes.append(obj)
-        update_view_print(f" {obj.name}")
+        update_view_print(f"{tab()}{obj.name}")
 
 
     for obj in meshes:
@@ -102,17 +126,19 @@ def export_armature_with_its_geometry_to_ue(rig):
         bpy.ops.object.make_single_user(object=True, obdata=True)
         bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
         bpy.ops.object.visual_transform_apply()
+        update_view_print(f"{tab()}Made single user and applied visual transform to {obj.name}")
 
-        update_view_print(f"Made single user and applied visual transform to {obj.name}")
+        if DONT_FIX_NORMALS not in obj.keys() or obj[DONT_FIX_NORMALS] is False:
+            fix_object_normals(obj)
+            update_view_print(f"{tab()}Normals fixed for {obj.name}")
         
         obj.data.name = f"{obj.name}__unique_mesh"
-
-        update_view_print(f"Renamed object data to {obj.data.name}")
+        update_view_print(f"{tab()}Renamed object data to {obj.data.name}")
 
         obj.select_set(False)
         deselect_everything()
 
-        update_view_print(f"Finished for {obj.name}")
+        update_view_print(f"{tab()}Finished for {obj.name}")
 
 
 
@@ -120,7 +146,7 @@ def export_armature_with_its_geometry_to_ue(rig):
     # bpy.ops.wm.send2ue()
 
     # delete the "Export" collection
-    # bpy.data.collections.remove(temporal_export_collection)
+    bpy.data.collections.remove(temporal_export_collection)
     move_to_collection_with_nierarchy(rig, original_collection_of_the_rig)
     update_view_print(f"Moved {rig.name} to {original_collection_of_the_rig.name}")
     update_view_print(f"Exported rig and its childs {rig.name} finished")
