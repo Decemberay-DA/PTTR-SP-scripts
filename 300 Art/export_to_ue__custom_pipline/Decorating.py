b990aa4410f02c8eb2d435c4ca83a53131a5267b
Ygor Boiko Mikhailovich <113383645+Rrailnechex@users.noreply.github.com>
2024-06-26 00:38:52 +0300

    everything is merged in to one file cz addons is trash)

--- 300 Art/export_to_ue__custom_pipline/Decorating.py
+++
@@ -1,5 +0,0 @@
-def returns_self(method):
-    def wrapper(self, *args, **kwargs):
-        method(self, *args, **kwargs)
-        return self
-    return wrapper
--- 300 Art/export_to_ue__custom_pipline/ExporterScript.py
+++ 300 Art/export_to_ue__custom_pipline/ExporterScript.py
@@ -1,27 +1,352 @@
-import sys
-import os
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-if script_dir not in sys.path:
-    sys.path.append(script_dir)
-
+import datetime
+import traceback
 import bpy
-import Logging
-from types import *
-import BlenderEX as b
-import ConfigLoader
+import json
+from types import SimpleNamespace
+from typing import *
 
-config = ConfigLoader.load_config().logging
+
+
+
+
+
+# Utils.py
+class Utils:
+    @staticmethod
+    def map_action(iterator, func):
+        for item in iterator:
+            func(item)
+
+    @staticmethod
+    def map_iter(iterator, func):
+        for item in iterator:
+            yield func(item)
+
+    @staticmethod
+    def map_iter_pipe(iterator, *funcs):
+        for item in iterator:
+            for func in funcs:
+                item = func(item)
+            yield item
+
+    @staticmethod
+    def execute_pass_queue(obj, *fn):
+        for fn in fn:
+            fn(obj)
+
+    @staticmethod
+    def do(obj, fn):
+        fn()
+        return obj
+
+    @staticmethod
+    def btw(fn):
+        fn()
+        return lambda obj: obj
+
+
+
+
+
+
+
+
+
+# Decorating.py
+class Decorating:
+    @staticmethod
+    def returns_self(method):
+        def wrapper(self, *args, **kwargs):
+            method(self, *args, **kwargs)
+            return self
+        return wrapper
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ConfigLoader.py
+class ConfigLoader:
+    config_file_json = """
+    {
+        "main": {
+            "is_revert_changes": false,
+            "is_export_to_ue": false
+        },
+        "logging": {
+            "tab_size": 3,
+            "this_file_name": "ExporterScript",
+            "log_file_name": "ExporterScript.log"
+        },
+        "passes": {
+            "fix_normals": {
+                "enabled": true,
+                "attribute_name": "dont_fix_normals"
+            },
+            "create_micro_bone": {
+                "enabled": true,
+                "attribute_name": "create_micro_bone",
+                "micro_bone_parent": "micro_bone_parent",
+                "micro_bone_name": "micro_bone_name"
+            },
+            "apply_render_geometry_modifiers": {
+                "enabled": false,
+                "attribute_name": "is_apply_render_or_view_modifiers"
+            }
+        }
+    }
+    """
+    @staticmethod
+    def load_config():
+        # with open("config.json", "r") as config_file:
+        #     config_dict = json.load(config_file)
+        config_dict = json.load(ConfigLoader.config_file_json)
+
+        def dict_to_namespace(d):
+            return json.loads(json.dumps(d), object_hook=lambda d: SimpleNamespace(**d))
+        
+        return dict_to_namespace(config_dict)
+
+config = ConfigLoader.load_config()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Logging:
+    def tab():
+        return " " * config.tab_size
+
+    def clear_log_file():
+        with open(config.logging.log_file_name, "w") as f:
+            pass
+    def log_to_file(message):
+        time_written = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+        stack_depth = len(traceback.extract_stack()) - 1 - 10
+        indentation = Logging.tab() * stack_depth
+        log_message = f"{time_written}: {indentation}{message}"
+        
+        with open(config.logging.log_file_name, "a") as f:
+            f.write(f"{log_message}\n")
+
+    class LoggerMonad:
+        def __init__(self, log_file_name):
+            self.log_file_name = log_file_name
+            self.current_intent = 0
+
+        @property
+        def current_intent(self):
+            return self._current_intent
+        
+        @property
+        def current_intent(self, level):
+            self._current_intent = level
+        
+        @property
+        def log_file_name(self):
+            return self.log_file_name
+
+        @Decorating.returns_self
+        def clear_log_file(self):
+            Logging.cLoggingear_log_file()
+
+        @Decorating.returns_self
+        def write(self, message):
+            Logging.log_to_file(message)
+        
+        @Decorating.returns_self 
+        def up(self):
+            self.current_intent += 1
+        
+        @Decorating.returns_self    
+        def down(self):
+            self.current_intent -= 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# BlenderEX.py
+class BlenderEX:
+    @staticmethod
+    def get_root_bone_of_armature(armature):
+        return armature.data.edit_bones[armature.data.edit_bones.keys()[0]]
+
+    @staticmethod
+    def deselect_everything():
+        bpy.ops.object.select_all(action='DESELECT')
+
+    @staticmethod
+    def iter_hierarchy_inclusive(obj):
+        yield obj
+        for child in obj.children:
+            yield from BlenderEX.iter_hierarchy_inclusive(child)
+    @staticmethod
+    def move_to_collection(obj, target_collection):
+        for col in obj.users_collection:
+            col.objects.unlink(obj)
+        target_collection.objects.link(obj)
+
+    @staticmethod
+    def move_to_collection_with_nierarchy(obj, target_collection):
+        Utils.map_action(
+            BlenderEX.iter_hierarchy_inclusive(obj), 
+            lambda x: BlenderEX.move_to_collection(x, target_collection)
+        )
+
+    @staticmethod
+    def update_view():
+        bpy.context.view_layer.update()
+        bpy.ops.wm.redraw_timer(type='DRAW_WIN_SWAP', iterations=1)
+
+    @staticmethod
+    def update_view_print(message):
+        BlenderEX.update_view()
+        print(message)
+        Logging.log_to_file(message)
+
+    # Select the object. I SAD: SELECT THE OBJECT
+    @staticmethod
+    def force_select_object(obj):
+        if obj.name in bpy.context.view_layer.objects:
+            obj.select_set(True)
+            bpy.context.view_layer.objects.active = obj
+        else:
+            bpy.context.view_layer.objects.active = obj
+            obj.select_set(True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # passes ------------------------------------------------------------------
 
 def fix_object_normal_pass(obj):
     if config.passes.fix_normals.attribute_name not in obj.keys() or obj[config.passes.fix_normals.attribute_name] is False:
         fix_object_normals(obj)
-        b.update_view_print(f"Normals fixed for {obj.name}")
+        Utils.update_view_print(f"Normals fixed for {obj.name}")
 
 def fix_object_normals(obj):
-    b.force_select_object(obj)
+    Utils.force_select_object(obj)
     bpy.ops.object.mode_set(mode='EDIT')
     bpy.ops.mesh.select_all(action='SELECT')
     bpy.ops.mesh.normals_make_consistent(inside=False)
@@ -41,7 +366,7 @@ def add_bone_to_armature(armature, bone_name="Bone.001", head=(0, 0, 0), tail=(0
     if parent_bone is not None:
         new_bone.parent = parent_bone
     else:
-        new_bone.parent = b.get_root_bone_of_armature(armature)
+        new_bone.parent = Utils.get_root_bone_of_armature(armature)
 
     bpy.ops.object.mode_set(mode='OBJECT')
 
@@ -73,17 +398,17 @@ def create_micro_bone_pass(obj, rig):
         name = obj[mb_cnf.micro_bone_name]
 
     bone = add_bone_to_armature(rig, name, obj.location, obj.location, parent_bone)
-    b.update_view_print(f"Created micro bone {bone.name} for {obj.name}")
+    Utils.update_view_print(f"Created micro bone {bone.name} for {obj.name}")
 
     fill_object_with_vertex_weight(obj, bone.name, 1)
-    b.update_view_print(f"Filled vertex weight for {obj.name}")
+    Utils.update_view_print(f"Filled vertex weight for {obj.name}")
 
     pass
 
 def apply_render_geometry_modifiers_pass(obj):
     if config.passes.apply_render_geometry_modifiers.enabled:
         apply_render_geometry_modifiers(obj)
-        b.update_view_print(f"{Logging.tab()}Applied render modifiers to {obj.name}")
+        Utils.update_view_print(f"{Logging.tab()}Applied render modifiers to {obj.name}")
 
 def apply_render_geometry_modifiers(obj):
     if obj.modifiers:
@@ -109,17 +434,44 @@ def apply_render_geometry_modifiers(obj):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def run_export_pipline_for_rig(rig):
-    b.update_view_print(f"Exporting for {rig.name} started")
+    Utils.update_view_print(f"Exporting for {rig.name} started")
 
     original_collection_of_the_rig = rig.users_collection[0]
     temporal_export_collection = bpy.data.collections.new(name="Export")
     bpy.context.scene.collection.children.link(temporal_export_collection)
-    b.update_view_print(f"Created collection {temporal_export_collection.name}")
+    Utils.update_view_print(f"Created collection {temporal_export_collection.name}")
 
     # add rig and its childs to the "Export" collection
-    b.move_to_collection_with_nierarchy(rig, temporal_export_collection)
-    b.update_view_print(f"Hierarchy of {rig.name} moved to {temporal_export_collection.name}")
+    Utils.move_to_collection_with_nierarchy(rig, temporal_export_collection)
+    Utils.update_view_print(f"Hierarchy of {rig.name} moved to {temporal_export_collection.name}")
 
 
     # create game rig  ------------------------------------------------
@@ -129,7 +481,7 @@ def run_export_pipline_for_rig(rig):
     # processing meshes and exporting to unreal ------------------------------------------------
 
     meshes = []
-    b.update_view_print(f"Collecting meshes of {rig.name}:")
+    Utils.update_view_print(f"Collecting meshes of {rig.name}:")
     for obj in temporal_export_collection.all_objects:
         # only for geometry
         if not (
@@ -138,19 +490,19 @@ def run_export_pipline_for_rig(rig):
             and obj.type is not None 
             and obj.type == "MESH"
         ):
-            b.update_view_print(f"{Logging.tab()}Skipped {obj.name if obj else 'deez nust'}")
+            Utils.update_view_print(f"{Logging.tab()}Skipped {obj.name if obj else 'deez nust'}")
             continue
 
         meshes.append(obj)
-        b.update_view_print(f"{Logging.tab()}{obj.name}")
+        Utils.update_view_print(f"{Logging.tab()}{obj.name}")
 
 
 
 
     for obj in meshes:
-        b.force_select_object(obj)
+        Utils.force_select_object(obj)
 
-        b.update_view_print(f"Selected {obj.name}")
+        Utils.update_view_print(f"Selected {obj.name}")
 
         # Make single user and apply visual transform
         bpy.ops.object.make_single_user(object=True, obdata=True)
@@ -161,7 +513,7 @@ def run_export_pipline_for_rig(rig):
 
 
         bpy.ops.object.visual_transform_apply()
-        b.update_view_print(f"{Logging.tab()}Made single user and applied visual transform to {obj.name}")
+        Utils.update_view_print(f"{Logging.tab()}Made single user and applied visual transform to {obj.name}")
 
 
         fix_object_normal_pass(obj)
@@ -170,28 +522,51 @@ def run_export_pipline_for_rig(rig):
 
         
         obj.data.name = f"{obj.name}__unique_mesh"
-        b.update_view_print(f"{Logging.tab()}Renamed object data to {obj.data.name}")
+        Utils.update_view_print(f"{Logging.tab()}Renamed object data to {obj.data.name}")
 
         obj.select_set(False)
-        b.deselect_everything()
+        Utils.deselect_everything()
 
-        b.update_view_print(f"{Logging.tab()}Finished for {obj.name}")
+        Utils.update_view_print(f"{Logging.tab()}Finished for {obj.name}")
 
 
 
     # Send to Unreal Engine all things from the "Export" collection
     if config.main.is_export_to_ue:
-        b.update_view_print(f"Sending to Unreal started")
+        Utils.update_view_print(f"Sending to Unreal started")
         bpy.ops.wm.send2ue()
-        b.update_view_print(f"Sending to Unreal finished")
+        Utils.update_view_print(f"Sending to Unreal finished")
 
     # delete the "Export" collection
     bpy.data.collections.remove(temporal_export_collection)
-    b.move_to_collection_with_nierarchy(rig, original_collection_of_the_rig)
-    b.update_view_print(f"Moved {rig.name} to {original_collection_of_the_rig.name}")
-    b.update_view_print(f"Exported rig and its childs {rig.name} finished")
+    Utils.move_to_collection_with_nierarchy(rig, original_collection_of_the_rig)
+    Utils.update_view_print(f"Moved {rig.name} to {original_collection_of_the_rig.name}")
+    Utils.update_view_print(f"Exported rig and its childs {rig.name} finished")
 
     pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -213,9 +588,9 @@ def main():
 
     Logging.clear_log_file()
 
-    b.update_view_print("Saved the current Blender file")
+    Utils.update_view_print("Saved the current Blender file")
 
-    b.deselect_everything()
+    Utils.deselect_everything()
 
     # Get the objects to export
     rigs_to_export = [
@@ -227,8 +602,8 @@ def main():
     for rig in rigs_to_export:
         run_export_pipline_for_rig(rig)
 
-    b.update_view_print(f"All rigs exported")
-    b.update_view_print(f"SCRIPT FINISHED")
+    Utils.update_view_print(f"All rigs exported")
+    Utils.update_view_print(f"SCRIPT FINISHED")
 
     # Revert all changes to the Blender file
     if config.main.is_revert_changes:
