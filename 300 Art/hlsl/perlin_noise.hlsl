d80d8fe04fff184ffac43a54aaaee65d5da30cfe
Ygor Boiko Mikhailovich <113383645+Rrailnechex@users.noreply.github.com>
2024-08-04 21:08:03 +0300

    hlhl

--- 300 Art/hlsl/perlin_noise.hlsl
+++ 300 Art/hlsl/perlin_noise.hlsl
@@ -5,9 +5,23 @@ float PerlinNoise(float value, float frequency, int octaves, int seed)
     for (int octave = 0; octave < octaves - 1; octave++)
     {
         float v = value * frequency + float(seed) * 12.468;
-        float a = Noise(int(v), seed);
-        float b = Noise(int(v) + 1, seed);
-        float t = Fade(v - floor(v));
+
+        float noise_a = 0.0;
+        {
+            int n = int(v) + seed * 137;
+            n = n << 13 ^ n;
+            noise_a = (1.0 - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
+        }
+        float noise_b = 0.0;
+        {
+            int n = int(v) + 1 + seed * 137;
+            n = n << 13 ^ n;
+            noise_a = (1.0 - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
+        }
+        float a = noise_a;
+        float b = noise_b;
+        float fade = v - floor(v);
+        float t = fade * fade * fade * (fade * (fade * 6 - 15) + 10);;
         noiseSum += lerp(a, b, t) * amplitude;
         frequency *= 2;
         amplitude *= 0.5;
@@ -15,21 +29,8 @@ float PerlinNoise(float value, float frequency, int octaves, int seed)
     return noiseSum;
 }
 
-float Noise(int x, int seed)
-{
-    int n = x + seed * 137;
-    n = n << 13 ^ n;
-    return (1.0 - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
-}
-
-float Fade(float t)
-{
-    return t * t * t * (t * (t * 6 - 15) + 10);
-}
-
-
 result = float3(
-    PerlinNoise(value * -1.0, frequency, octaves, seed),
+    PerlinNoise(-value, frequency, octaves, seed),
     PerlinNoise(value + 465.321, frequency, octaves, seed),
     PerlinNoise(value, frequency, octaves, seed),
 );
